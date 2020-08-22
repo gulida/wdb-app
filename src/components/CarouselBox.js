1af2c2d97facd5b772bcaa706004550a78da7a59
Kimsanova <synak2013@gmail.com>
2020-08-22 18:31:20 +0600

    Slider carousel

---
+++ src/components/CarouselBox.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import frc from '../assets/1.jpeg'
+import frc2 from '../assets/2.jpeg'
+import frc3 from '../assets/3.jpeg'
+
+class CarouselBox extends Component {
+  render() {
+    return (
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              height="800"
+              className="d-block w-100"
+              src={frc}
+              alt="Frc"
+            />
+            <Carousel.Caption>
+              <h3>Frc Image</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, at!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              height="800"
+              className="d-block w-100"
+              src={frc2}
+              alt="Frc2"
+            />
+            <Carousel.Caption>
+              <h3>Frc Image</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, at!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              height="800"
+              className="d-block w-100"
+              src={frc3}
+              alt="Frc3"
+            />
+            <Carousel.Caption>
+              <h3>Frc Image</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, at!</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default CarouselBox;
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,74 @@
 import React, {Component} from 'react';
-import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import {
+  Button, Container, Form,
+  FormControl, Nav, Navbar} from "react-bootstrap";
 import logo from './logo192.png'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link} from "react-router-dom";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contacts from "../pages/Contacts";
+import Blog from "../pages/Blog";
 
 class Header extends Component {
-    render() {
-        return (
-            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            src={logo}
-                            height="30"
-                            width="30"
-                            className="d-inline-block align-top"
-                            alt="Logo"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/about">About us</Nav.Link>
-                            <Nav.Link href="/contacts">Contacts</Nav.Link>
-                            <Nav.Link href="/blog">Blog</Nav.Link>
-                        </Nav>
-                        <Form inline>
-                            <FormControl
-                                type="text"
-                                placeholder="Search"
-                                className="mr-sm-2"
-                            />
-                            <Button variant="outline-info">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        );
-    }
+  render() {
+    return (
+      <>
+
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                  src={logo}
+                  height="30"
+                  width="30"
+                  className="d-inline-block align-top"
+                  alt="Logo"
+              />
+              React Site
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About us</Nav.Link>
+                <Nav.Link href="/contacts">Contacts</Nav.Link>
+                <Nav.Link href="/blog">Blog</Nav.Link>
+              </Nav>
+
+              <Form inline>
+                <FormControl
+                    type="text"
+                    placeholder="Search"
+                    className="mr-sm-2"
+                />
+
+                <Button variant="outline-info">Search</Button>
+
+              </Form>
+
+            </Navbar.Collapse>
+
+          </Container>
+
+        </Navbar>
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route exact path="/blog" component={Blog} />
+          </Switch>
+        </Router>
+
+      </>
+    );
+  }
 }
 
 export default Header;
