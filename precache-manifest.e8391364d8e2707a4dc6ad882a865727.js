f71e648aa64bb86e72ce093ee5351fd3d1faf44d
Kimsanova <synak2013@gmail.com>
2020-08-23 18:51:34 +0600

    Updates

---
+++ precache-manifest.e8391364d8e2707a4dc6ad882a865727.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0134fb6aa0fb87f33fd4e5e8f26244b4",
+    "url": "/wdb-app/index.html"
+  },
+  {
+    "revision": "d2c61d81d5b017bcf828",
+    "url": "/wdb-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "19afa0b6dd7b6b479a37",
+    "url": "/wdb-app/static/css/main.bd04a2c4.chunk.css"
+  },
+  {
+    "revision": "d2c61d81d5b017bcf828",
+    "url": "/wdb-app/static/js/2.315d11d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wdb-app/static/js/2.315d11d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19afa0b6dd7b6b479a37",
+    "url": "/wdb-app/static/js/main.be095589.chunk.js"
+  },
+  {
+    "revision": "b9d62410753f5335377e",
+    "url": "/wdb-app/static/js/runtime-main.1f0e35c4.js"
+  },
+  {
+    "revision": "435800636785d63ec3ef2b549f38a0ef",
+    "url": "/wdb-app/static/media/1.43580063.jpeg"
+  },
+  {
+    "revision": "2afc9668d5aedd6b840098ceb69effcf",
+    "url": "/wdb-app/static/media/2.2afc9668.jpeg"
+  },
+  {
+    "revision": "e196b95ab52a785167bb5a525f4858e7",
+    "url": "/wdb-app/static/media/3.e196b95a.jpeg"
+  },
+  {
+    "revision": "1b2e84bae100bedaa4f85a093dcd9e7a",
+    "url": "/wdb-app/static/media/4.1b2e84ba.jpeg"
+  },
+  {
+    "revision": "617b40be3fbf4c3b729578bc4bdd484c",
+    "url": "/wdb-app/static/media/5.617b40be.jpeg"
+  },
+  {
+    "revision": "af431837a8e80a8a8d53743c0c30944f",
+    "url": "/wdb-app/static/media/6.af431837.jpeg"
+  },
+  {
+    "revision": "e39410883f5be128c2cf55440876432b",
+    "url": "/wdb-app/static/media/aizat.e3941088.jpg"
+  },
+  {
+    "revision": "c9782facd8e7d5249fa50c5cfe3db328",
+    "url": "/wdb-app/static/media/gulasel.c9782fac.jpg"
+  },
+  {
+    "revision": "d62088839f91b8efa55735011354e245",
+    "url": "/wdb-app/static/media/kg-math.d6208883.jpg"
+  },
+  {
+    "revision": "118a776504fc625cb8156f58c586fdfb",
+    "url": "/wdb-app/static/media/logo3.118a7765.png"
+  },
+  {
+    "revision": "d9e974c56df70301447b8b0be3ecaf69",
+    "url": "/wdb-app/static/media/ru-bio.d9e974c5.jpg"
+  },
+  {
+    "revision": "a0192259f118009dad9e0d389cec3ba1",
+    "url": "/wdb-app/static/media/ru-lang.a0192259.jpg"
+  },
+  {
+    "revision": "b2e25786b1937999d324d0d26652f4d2",
+    "url": "/wdb-app/static/media/sb-osn.b2e25786.jpg"
+  },
+  {
+    "revision": "7e5bbd581417f38fa1c21716f5c3b66e",
+    "url": "/wdb-app/static/media/sb-pred.7e5bbd58.jpg"
+  },
+  {
+    "revision": "a8e6ccd6495bf662d45276afee75973c",
+    "url": "/wdb-app/static/media/shirin.a8e6ccd6.png"
+  },
+  {
+    "revision": "0099a4d3b69564b8d9a7020d876e7bab",
+    "url": "/wdb-app/static/media/zhamilia.0099a4d3.jpg"
+  }
+]);
