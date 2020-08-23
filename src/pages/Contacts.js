621538711095a2b8bad834e5a24c35a10b1e1f9a
Kimsanova <synak2013@gmail.com>
2020-08-23 14:48:14 +0600

    Contact us page, Form, Form.Group, Form.Label, Form.Check, Form.Control

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import {Button, Container, Form} from "react-bootstrap";
 
 class Contacts extends Component {
   render() {
     return (
-      <div>
-        <h1>Contacts</h1>
-      </div>
+        <Container style={{width: '500px', textAlign: 'left'}}>
+          <h1 className="text-center">Contact us</h1>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text>
+                We'll never share your email with anyone else
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Example text area</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Submit</Button>
+
+          </Form>
+        </Container>
     );
   }
 }
